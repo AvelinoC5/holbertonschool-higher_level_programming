c01e5acb8cb34af216323ea093a16a3d04a3219c
AvelinoC5 <avelino_63@hotmail.com>
2022-09-22 20:52:22 -0500

    update 3 task

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,11 +2,13 @@
 import numbers
 from sys import argv
 
+
 def run():
     numbers = 0
     for indx in argv[1:]:
         numbers += int(indx)
     print("{}".format(numbers))
+
 
 if __name__ == "__main__":
     run()
