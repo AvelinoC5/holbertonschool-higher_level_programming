6e7664b13f5e0f1a529dea32fc6a46fe99688a4e
AvelinoC5 <avelino_63@hotmail.com>
2022-09-22 20:50:36 -0500

    update 3 task

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import numbers
+from sys import argv
+
+def run():
+    numbers = 0
+    for indx in argv[1:]:
+        numbers += int(indx)
+    print("{}".format(numbers))
+
+if __name__ == "__main__":
+    run()
