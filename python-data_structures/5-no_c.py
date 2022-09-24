42e830ca368ec3ca8387f3fb169e5a5e7c4bbc0e
AvelinoC5 <avelino_63@hotmail.com>
2022-09-23 20:04:43 -0500

    update 5 task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string) - 2):
-        if (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_string = my_string[:i] + "" + my_string[i+1:]
-    return (my_string)
+    remove = ""
+    for a in my_string:
+        if a not in 'cC':
+            remove += a
+    return remove
