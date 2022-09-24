72af8e0279c06e0d0af5db18f31d30d7f8fa0fff
AvelinoC5 <avelino_63@hotmail.com>
2022-09-23 23:14:09 -0500

    update 6 task

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    no_c = {ord('c'): "", ord('C'): ""}
-    return (my_string.translate(no_c))
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        s = ""
+        for y in x:
+            print("{:s}{:d}".format(s, y), end='')
+            s = " "
+        print()
