93fcc31f04c6ea21085a5826bb474f0a70ab82c3
AvelinoC5 <avelino_63@hotmail.com>
2022-09-30 17:58:28 -0500

    update 1 task

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a class Square that defines a square by: (based on 0-square.py)
+"""
+
+class Square:
+    """ Square class.
+    def __init__(self, size):  
+    """
+
+    """
+    Initialization self.size
+    """
+self.__size = size
