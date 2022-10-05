e3b6c00ec30114020adb7973e845d9ccc8883bbc
AvelinoC5 <avelino_63@hotmail.com>
2022-10-04 21:54:48 -0500

    update 0 task

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Returns an integer: the addition of a and b
+Function validates the type int or float
+Otherwise, raise error, Cast values too
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds the input integers
+    """
+    if (type(a) is float):
+        a = int(a)
+    if (type(b) is float):
+        b = int(b)
+    if not ((type(a) is int) or (type(a) is float)):
+        raise TypeError('a must be an integer')
+    if not ((type(b) is int) or (type(b) is float)):
+        raise TypeError('b must be an integer')
+    return (a + b)
