56e3cf195462c7367bc9e90980bad05c0041cea0
AvelinoC5 <avelino_63@hotmail.com>
2022-10-14 07:50:34 -0500

    update 3 task

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Program that checks if a class is the same
+object os inherit from """
+
+
+def is_kind_of_class(obj, a_class):
+    """  function that returns True if the object is an instance 
+    of ; otherwise False. """
+    return (isinstance(obj, a_class))
