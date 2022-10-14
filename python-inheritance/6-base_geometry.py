ad76912d470569303a07f0dbda6d7ac4a4aadf1e
AvelinoC5 <avelino_63@hotmail.com>
2022-10-14 14:10:31 -0500

    update 6 task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Empty Geometry Class """
+
+
+class BaseGeometry:
+    """ class that improve geometry"""
+    def area(self):
+        """ raises an Exception with message area() is not implemented """
+        raise Exception("area() is not implemented")
