3e856dae6e224fe08acaa4b67b75200fe349ce64
AvelinoC5 <avelino_63@hotmail.com>
2022-10-13 22:47:52 -0500

    update 2 task

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Program that validate 
+that object is exact the same """
+
+
+def is_same_class(obj, a_class):
+    if (type(obj) == a_class):
+        return True
+    return False
