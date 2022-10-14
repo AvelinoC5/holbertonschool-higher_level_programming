42dd8034d3b603db3aec4b21214dfc01fa74538c
AvelinoC5 <avelino_63@hotmail.com>
2022-10-14 15:28:33 -0500

    update 7 task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-""" Program that Load, add, save all arguments to Python list,
-and then save them to a file """
+"""
+Program Load, add, save all arguments to a Python list,
+and then save them to a file
+"""
 import sys
-import os
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-if __name__ == "__main__":
-    data = "add_item.json"
-
-    if os.path.isfile(data):
-        my_list = load_from_json_file(data)
-    else:
-        my_list = []
-
-    for i in range(1, len(sys.argv)):
-        my_list.append(sys.argv[i])
-    save_to_json_file(my_list, data)
+open("add_item.json", "a")
+try:
+    file = load_from_json_file("add_item.json")
+except (ValueError):
+    file = []
+save_to_json_file(file + sys.argv[1:], "add_item.json")
