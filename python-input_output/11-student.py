6d831d3767bb8ec93516004695e54a47bd8eb146
AvelinoC5 <avelino_63@hotmail.com>
2022-10-14 22:56:24 -0500

    update 11 task

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
-""" Program that define a Student with filter """
+"""
+Student class Initialization for first_name,
+last_name and age Return dictionary for the object
+"""
 
 
 class Student:
-    """ class Student that defines a student """
+    """
+    Student class
+    """
+
     def __init__(self, first_name, last_name, age):
+        """
+        Initialization class
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ retrieves a dictionary representation of a Student instance """
-        dic = {}
-        if (type(attrs) is not list):
-            return (self.__dict__)
-        else:
-            for i in attrs:
-                if (i in self.__dict__):
-                    dic[i] = self.__dict__[i]
-            return (dic)
+        """
+        Return description of the dictionary for JSON searilaztion
+        verification if attrs is a list
+        """
+        if type(attrs) is list and all([type(idx) == str for idx in attrs]):
+            return {lr: lr_2 for lr, lr_2
+                    in self.__dict__.items() if lr in attrs}
+
+        return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """
+        Replace all attributes of students
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
