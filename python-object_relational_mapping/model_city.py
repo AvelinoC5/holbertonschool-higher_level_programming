651d5b6d9a3f3ce107fcdaa33657474f1c1f7dc6
AvelinoC5 <avelino_63@hotmail.com>
2022-11-17 19:44:35 -0500

    add model city  task

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module that contains the class definition of a City
+    and an instance Base = declarative_base().
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+
+class City(Base):
+    """ Declare Class Base that inherits from Base """
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
