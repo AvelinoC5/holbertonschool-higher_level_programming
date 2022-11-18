27fc3785b8d85f924512ded1558fa076ec93317a
AvelinoC5 <avelino_63@hotmail.com>
2022-11-17 19:42:27 -0500

    add model task

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """This module that contains the class definition of a State
     and an instance Base = declarative_base().
-   Created on Sunday, December 13, 2022
-   @author: DaisyG Chipana Lapa
 """
 
 from sqlalchemy import create_engine
