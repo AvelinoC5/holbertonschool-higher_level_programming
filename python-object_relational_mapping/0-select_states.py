234a00fdec3036562fd4cb7e1ab43b18056006eb
AvelinoC5 <avelino_63@hotmail.com>
2022-11-18 19:15:18 -0500

    update 0 task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-''' Script to retrieve data from a database
-Created on Nov 17 09:33:11 2022
-@author: Avelino Carvajal'''
+"""This module lists all states with a name starting with N (upper N)
+   from the database hbtn_0e_0_usa
+"""
 
-import sys
-import MySQLdb
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3],
-                        host='localhost', port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id")
-    [print(state) for state in cur.fetchall()]
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states"
+                " WHERE BINARY name LIKE BINARY UPPER('N%') ORDER BY id ASC ")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
-    db.close()
+    conn.close()
