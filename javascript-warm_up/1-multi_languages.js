99362e1935d57802fd26f88635e7211c548f98d5
AvelinoC5 <avelino_63@hotmail.com>
2022-12-19 15:21:15 -0500

    update file 1 task

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-C_is = "C is fun";
-Pyth = "Python is cool";
-Java = "JavaScript is amazing";
+C_is = 'C is fun';
+Pyth = 'Python is cool';
+Java = 'JavaScript is amazing';
 console.log(C_is, Pyth, Java);
