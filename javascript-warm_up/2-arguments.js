90fc6cb7894fa7e3fdb364c3ca1cbf15fca8c477
AvelinoC5 <avelino_63@hotmail.com>
2022-12-19 15:49:19 -0500

    update file 2 task

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 2) {
+const process = require("process");
+
+const len = process.argv.length;
+
+if (len === 2) {
   console.log("No argument");
-} else if (process.argv.length === 3) {
+} else if (len === 3) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
