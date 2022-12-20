34e65264e5fb023ea612aff42156399cf31fd6f5
AvelinoC5 <avelino_63@hotmail.com>
2022-12-19 20:13:06 -0500

    update file 9 task

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-function add (a, b) {
-    const process = require('process');
-  
-    a = parseInt(process.argv[2]);
-    b = parseInt(process.argv[3]);
-  
-    console.log(a + b);
-  } add();
+function add(a, b) {
+  const process = require('process');
+
+  a = parseInt(process.argv[2]);
+  b = parseInt(process.argv[3]);
+
+  console.log(a + b);
+}
+add();
