38d3b6b99fba36bea7a7d997bdd7fccc15c7697d
AvelinoC5 <avelino_63@hotmail.com>
2022-12-20 21:01:30 -0500

    created 3 file

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,13 +9,13 @@ class Rectangle {
   }
 
   print () {
-    let output = "";
+    let output = '';
 
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        output += "X";
+        output += 'X';
       }
-      output += "\n";
+      output += '\n';
     }
     console.log(output.trim());
   }
