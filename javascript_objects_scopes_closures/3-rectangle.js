89dcaa9a9177b03a957b7cd01d0dd95b62b8d9ca
AvelinoC5 <avelino_63@hotmail.com>
2022-12-20 20:54:24 -0500

    created 3 file

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let output = "";
+
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        output += "X";
+      }
+      output += "\n";
+    }
+    console.log(output.trim());
+  }
+}
+
+module.exports = Rectangle;
