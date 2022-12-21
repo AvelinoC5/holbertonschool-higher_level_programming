c8d292f4c68d77579443b9f51e0a2e8c809814ea
AvelinoC5 <avelino_63@hotmail.com>
2022-12-20 21:25:53 -0500

    update 6 file

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,27 +1,10 @@
 #!/usr/bin/node
-
-const Rectangle = require('./5-rectangle');
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    if (!c) {
-      c = 'X';
+module.exports = class Square extends require("./5-square") {
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
-
-    let output = '';
-
-    for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        output += c;
-      }
-      output += '\n';
-    }
-    console.log(output.trim());
   }
-}
-
-module.exports = Square;
+};
