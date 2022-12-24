6fb823e3f03fac3461fc918f8c1c7f42d47cd6ea
AvelinoC5 <avelino_63@hotmail.com>
2022-12-23 21:52:30 -0500

    created 3-file

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-const axios = require('axios');
-const Id = process.argv[2];
-const IdURL = `https://swapi-api.hbtn.io/api/films/${Id}`;
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const request = require('request');
 
-axios.get(IdURL).then((response) => {
-  console.log(response.data.title);
+request.get(url, (error, response, data) => {
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(data).title);
+  }
 });
