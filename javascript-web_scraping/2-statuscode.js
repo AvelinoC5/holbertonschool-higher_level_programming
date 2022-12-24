de62f7bd639a45f6793cebdd16c456e14e8e60fe
AvelinoC5 <avelino_63@hotmail.com>
2022-12-23 21:47:32 -0500

    created 2-file

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,11 @@ const axios = require('axios').default;
 const Processes = require('process');
 const URL = Processes.argv[2];
 
-axios.get(URL).then(function (response) {
-  console.log('code: ' + response.status);
-})
+axios
+  .get(URL)
+  .then(function (response) {
+    console.log('code: ' + response.status);
+  })
   .catch(function (error) {
     if (error.response) {
       console.log('code: ' + error.response.status);
